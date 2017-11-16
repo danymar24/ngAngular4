f3b57bef45978ce29c3aadab30ab56e2e1f8f39c
Rodriguez <DanielRodriguez1@thomsonreuters.com>
2017-11-16 12:27:55 -0600

    generated add page component and added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AddPageComponent } from './components/add-page/add-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPageComponent
   ],
   imports: [
     BrowserModule
